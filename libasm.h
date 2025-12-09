1c89498157aea3b1cec761248a38a8de4fd5ab53
Damien Puccioni <57726808+dpuccion@users.noreply.github.com>
2025-12-09 20:39:47 +0100

    Update libasm.h

--- libasm.h
+++ libasm.h
@@ -1,13 +1,16 @@
 #ifndef LIBASM_H
 # define LIBASM_H
 
+#include <stddef.h>
+#include <sys/types.h>
 #include <stdlib.h>
 #include <stdio.h>
 
-extern	size_t	ft_strlen(char *s);
-// extern	char	*ft_strdup(char *s);
-extern	char	*ft_strcpy(char *s1, char *s2);
-extern	int		ft_strcmp(char *s1, char *s2);
-
+size_t    ft_strlen(const char *s);
+char     *ft_strcpy(char *dest, const char *src);
+int       ft_strcmp(const char *s1, const char *s2);
+ssize_t   ft_write(int fd, const void *buf, size_t count);
+ssize_t   ft_read(int fd, void *buf, size_t count);
+char     *ft_strdup(const char *s);
 
 #endif
